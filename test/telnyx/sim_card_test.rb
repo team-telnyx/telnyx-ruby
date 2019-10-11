5a245d9895f3e9b836f9e3aada77af21f1df922e
lunarfyre7 <gvandereay@gmail.com>
2019-10-10 18:09:20 -0700

    Add more actions to sim card endpoint

--- test/telnyx/sim_card_test.rb
+++ test/telnyx/sim_card_test.rb
@@ -12,16 +12,45 @@ module Telnyx
       assert_kind_of Telnyx::SimCard, sim
     end
 
-    should "deactivate" do
+    should "list sim cards" do
+      stub = stub_request(:get, "#{Telnyx.api_base}/v2/sim_cards")
+             .to_return(body: JSON.generate(data: (1..5).map { |i| { record_type: "sim_card", id: i } }))
+      simlist = Telnyx::SimCard.list
+      assert_requested stub
+      assert_kind_of Telnyx::ListObject, simlist
+    end
+
+    should "save sim card" do
       stub_request(:get, "#{Telnyx.api_base}/v2/sim_cards/123")
         .to_return(body: JSON.generate(data: { record_type: "sim_card", id: "123" }))
+      stub = stub_request(:patch, "#{Telnyx.api_base}/v2/sim_cards/123")
+             .to_return(body: JSON.generate(data: { record_type: "sim_card", id: "123" }))
+    end
 
-      stub = stub_request(:post, "#{Telnyx.api_base}/v2/sim_cards/123/actions/deactivate")
-             .to_return(body: JSON.generate(errors: []), status: 202)
+    context "actions" do
+      should "deactivate" do
+        stub_request(:get, "#{Telnyx.api_base}/v2/sim_cards/123")
+          .to_return(body: JSON.generate(data: { record_type: "sim_card", id: "123" }))
 
-      sim = Telnyx::SimCard.retrieve "123"
-      sim.deactivate
-      assert_requested stub
+        stub = stub_request(:post, "#{Telnyx.api_base}/v2/sim_cards/123/actions/deactivate")
+               .to_return(body: JSON.generate(errors: []), status: 202)
+
+        sim = Telnyx::SimCard.retrieve "123"
+        sim.deactivate
+        assert_requested stub
+      end
+
+      should "activate" do
+        stub_request(:get, "#{Telnyx.api_base}/v2/sim_cards/123")
+          .to_return(body: JSON.generate(data: { record_type: "sim_card", id: "123" }))
+
+        stub = stub_request(:post, "#{Telnyx.api_base}/v2/sim_cards/123/actions/activate")
+               .to_return(body: JSON.generate(errors: []), status: 202)
+
+        sim = Telnyx::SimCard.retrieve "123"
+        sim.activate
+        assert_requested stub
+      end
     end
   end
 end
