96904533de06ece1cb6fff9b352ef40cefa24e8a
lunarfyre7 <gvandereay@gmail.com>
2019-10-16 20:45:39 -0700

    tweaks

--- lib/telnyx/phone_number.rb
+++ lib/telnyx/phone_number.rb
@@ -6,14 +6,14 @@ module Telnyx
     extend Telnyx::APIOperations::List
     extend Telnyx::APIOperations::NestedResource
 
-    ACTIONS = %w[voice messaging].freeze
-
-    ACTIONS.each do |action|
-      nested_resource_class_methods action,
-                                    path: [action],
-                                    operations: %i[create list],
-                                    instance_methods: { list: action, create: "create_#{action}" }
-    end
+    nested_resource_class_methods "voice",
+                                  path: ["voice"],
+                                  operations: %i[create list],
+                                  instance_methods: { list: "voice", create: "update_voice" } # Using create instead of update because the endpoint requires a put, not patch request
+    nested_resource_class_methods "messaging",
+                                  path: ["messaging"],
+                                  operations: %i[list],
+                                  instance_methods: { list: "messaging" }
 
     OBJECT_NAME = "phone_number".freeze
   end
