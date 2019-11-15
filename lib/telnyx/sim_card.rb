a9c57e455b4148c9657044ba80e67daad491107e
lunarfyre7 <gvandereay@gmail.com>
2019-11-15 12:30:30 -0800

    add register action

--- lib/telnyx/sim_card.rb
+++ lib/telnyx/sim_card.rb
@@ -14,6 +14,11 @@ module Telnyx
                                     instance_methods: { create: action }
     end
 
+    def self.register(params = {}, opts = {})
+      resp, opts = request(:post, "/v2/actions/register/sim_cards", params, opts)
+      Util.convert_to_telnyx_object(resp.data, opts)
+    end
+
     OBJECT_NAME = "sim_card".freeze
   end
 end
