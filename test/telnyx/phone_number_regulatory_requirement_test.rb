02a454384432531ab841cb2f6a612f4bff6da69e
d-telnyx <68876596+d-telnyx@users.noreply.github.com>
2021-03-29 17:34:48 -0400

    updating version information for release (#59)
    * updating version information for release
    
    * update path for phone_numbers_regulatory_requirements

--- test/telnyx/phone_number_regulatory_requirement_test.rb
+++ test/telnyx/phone_number_regulatory_requirement_test.rb
@@ -7,7 +7,7 @@ module Telnyx
   class PhoneNumberRegulatoryRequirementTest < Test::Unit::TestCase
     should "be listable" do
       phone_number_reg_req = PhoneNumberRegulatoryRequirement.list
-      assert_requested :get, "#{Telnyx.api_base}/v2/phone_number_regulatory_requirements"
+      assert_requested :get, "#{Telnyx.api_base}/v2/phone_numbers_regulatory_requirements"
       assert_kind_of PhoneNumberRegulatoryRequirement, phone_number_reg_req.first
     end
   end
