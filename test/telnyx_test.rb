30e09b3694dcd0eea4caf32b9c935287c8046d60
Dan Tolbert <dant@telnyx.com>
2021-04-26 10:34:46 -0400

    fix rubocop violations

--- test/telnyx_test.rb
+++ test/telnyx_test.rb
@@ -4,33 +4,29 @@ require ::File.expand_path("../test_helper", __FILE__)
 
 class TelnyxTest < Test::Unit::TestCase
   should "allow app_info to be configured" do
-    begin
-      old = Telnyx.app_info
-      Telnyx.set_app_info(
-        "MyAwesomePlugin",
-        partner_id: "partner_1234",
-        url: "https://myawesomeplugin.info",
-        version: "1.2.34"
-      )
-      assert_equal({
-        name: "MyAwesomePlugin",
-        partner_id: "partner_1234",
-        url: "https://myawesomeplugin.info",
-        version: "1.2.34",
-      }, Telnyx.app_info)
-    ensure
-      Telnyx.app_info = old
-    end
+    old = Telnyx.app_info
+    Telnyx.set_app_info(
+      "MyAwesomePlugin",
+      partner_id: "partner_1234",
+      url: "https://myawesomeplugin.info",
+      version: "1.2.34"
+    )
+    assert_equal({
+      name: "MyAwesomePlugin",
+      partner_id: "partner_1234",
+      url: "https://myawesomeplugin.info",
+      version: "1.2.34",
+    }, Telnyx.app_info)
+  ensure
+    Telnyx.app_info = old
   end
 
   should "allow max_network_retries to be configured" do
-    begin
-      old = Telnyx.max_network_retries
-      Telnyx.max_network_retries = 99
-      assert_equal 99, Telnyx.max_network_retries
-    ensure
-      Telnyx.max_network_retries = old
-    end
+    old = Telnyx.max_network_retries
+    Telnyx.max_network_retries = 99
+    assert_equal 99, Telnyx.max_network_retries
+  ensure
+    Telnyx.max_network_retries = old
   end
 
   should "have default open and read timeouts" do
