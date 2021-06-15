d4479c227ad11e408d969f6877671eea8125e563
Jake Vandereay <gvandereay@gmail.com>
2021-06-15 15:29:10 -0700

    remove deprecation warning; allow MessagingPhoneNumber in object table

--- lib/telnyx/messaging_phone_number.rb
+++ lib/telnyx/messaging_phone_number.rb
@@ -5,11 +5,6 @@ module Telnyx
     include Telnyx::APIOperations::Save
     extend Telnyx::APIOperations::List
 
-    def initialize(*)
-      super
-      warn "[DEPRECATION] MessagingPhoneNumber is deprecated, use PhoneNumber instead"
-    end
-
     OBJECT_NAME = "messaging_phone_number".freeze
     def self.resource_path(id = nil)
       return "phone_numbers/#{CGI.escape(id)}/messaging" if id
