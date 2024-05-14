4fdb1f29fd724ac1626f7eecca7e443802e86178
ADandyGuyInSpace <vladi@telnyx.com>
2024-05-14 14:17:47 -0500

    omit wrong line

--- test/telnyx/summary_test.rb
+++ test/telnyx/summary_test.rb
@@ -4,8 +4,8 @@ require_relative "../test_helper"
 
 module Telnyx
   class SummaryTest < Test::Unit::TestCase
-    omit "method removed from operation"
     should "be creatable" do
+      omit "method removed from operation"
       Telnyx::Summary.create(bucket: "Foo")
       assert_requested :post, "#{Telnyx.api_base}/v2/ai/summarize"
     end
