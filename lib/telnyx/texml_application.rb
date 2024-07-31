380675234030f450898eb20a999f762175a92ddb
ADandyGuyInSpace <vladi@telnyx.com>
2024-07-31 09:58:01 -0500

    update texml_applications

--- lib/telnyx/texml_application.rb
+++ lib/telnyx/texml_application.rb
@@ -3,6 +3,10 @@
 module Telnyx
   class TexmlApplication < APIResource
     extend Telnyx::APIOperations::List
+    extend Telnyx::APIOperations::Create
+    extend Telnyx::APIOperations::Update
+    extend Telnyx::APIOperations::Retrieve
+    extend Telnyx::APIOperations::Delete
 
     OBJECT_NAME = "texml_application".freeze
   end
