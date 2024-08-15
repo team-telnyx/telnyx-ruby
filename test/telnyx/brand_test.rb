e61903f6e4bbf06a267bb15bdd4b9516bed3ed8d
ADandyGuyInSpace <vladi@telnyx.com>
2024-08-15 00:58:56 -0500

    test: update API endpoint for brand operations to remove '10dlc' prefix

--- test/telnyx/brand_test.rb
+++ test/telnyx/brand_test.rb
@@ -13,20 +13,20 @@ module Telnyx
         registration_status: "REGISTERED",
         vertical: "COMMUNICATIONS"
       )
-      assert_requested :post, "#{Telnyx.api_base}/10dlc/brand"
+      assert_requested :post, "#{Telnyx.api_base}/brand"
       assert brand.is_a?(Telnyx::Brand)
     end
 
     should "be listable" do
       brand = Telnyx::Brand.list
-      assert_requested :get, "#{Telnyx.api_base}/10dlc/brand"
+      assert_requested :get, "#{Telnyx.api_base}/brand"
       assert brand.data.is_a?(Array)
       assert brand.data[0].is_a?(Telnyx::Brand)
     end
 
     should "be retrievable" do
       brand = Telnyx::Brand.retrieve("123")
-      assert_requested :get, "#{Telnyx.api_base}/10dlc/brand/123"
+      assert_requested :get, "#{Telnyx.api_base}/brand/123"
       assert brand.is_a?(Telnyx::Brand)
     end
 
@@ -34,41 +34,41 @@ module Telnyx
       brand = Telnyx::Brand.retrieve("123")
       brand.name = "Updated Brand"
       brand.save
-      assert_requested :patch, "#{Telnyx.api_base}/10dlc/brand/123"
+      assert_requested :patch, "#{Telnyx.api_base}/brand/123"
     end
 
     should "be deletable" do
       brand = Telnyx::Brand.retrieve("123")
       brand.delete
-      assert_requested :delete, "#{Telnyx.api_base}/10dlc/brand/123"
+      assert_requested :delete, "#{Telnyx.api_base}/brand/123"
     end
     should "get feedback" do
       brand_feedback = Telnyx::Brand.get_feedback("123")
-      assert_requested :get, "#{Telnyx.api_base}/10dlc/brand/123/feedback"
+      assert_requested :get, "#{Telnyx.api_base}/brand/123/feedback"
       assert brand_feedback.is_a?(Telnyx::TelnyxObject)
     end
 
     should "list external vettings" do
       external_vettings = Telnyx::Brand.list_external_vettings("123")
-      assert_requested :get, "#{Telnyx.api_base}/10dlc/brand/123/externalVetting"
+      assert_requested :get, "#{Telnyx.api_base}/brand/123/externalVetting"
       assert external_vettings.is_a?(Telnyx::TelnyxObject)
     end
 
     should "order external vetting" do
       external_vetting = Telnyx::Brand.order_external_vetting("123", {})
-      assert_requested :post, "#{Telnyx.api_base}/10dlc/brand/123/externalVetting"
+      assert_requested :post, "#{Telnyx.api_base}/brand/123/externalVetting"
       assert external_vetting.is_a?(Telnyx::TelnyxObject)
     end
 
     should "import external vetting record" do
       external_vetting_record = Telnyx::Brand.import_external_vetting_record("123", {})
-      assert_requested :put, "#{Telnyx.api_base}/10dlc/brand/123/externalVetting"
+      assert_requested :put, "#{Telnyx.api_base}/brand/123/externalVetting"
       assert external_vetting_record.is_a?(Telnyx::TelnyxObject)
     end
 
     should "revet brand" do
       revet = Telnyx::Brand.revet("123")
-      assert_requested :put, "#{Telnyx.api_base}/10dlc/brand/123/revet"
+      assert_requested :put, "#{Telnyx.api_base}/brand/123/revet"
       assert revet.is_a?(Telnyx::TelnyxObject)
     end
   end
